dd78b4b198c53c67abea311c11a85ede40e03c25
RawandDev <rawand.200091384037@gmail.com>
2021-08-03 17:38:03 +0300

    done!

--- src/components/MoviesList.js
+++ src/components/MoviesList.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 function MoviesList({ movies }) {
   const displayMovies = Object.keys(movies).map((movieID) => (
     <li key={movieID}>
-      <Link to={`/movie/${movieID}`}>{movies[movieID].title}</Link>
+      <Link to={`/movies/${movieID}`}>{movies[movieID].title}</Link>
     </li>
   ));
 

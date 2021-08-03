b0ebe7d61d01006e0918371854288d80e2766395
RawandDev <rawand.200091384037@gmail.com>
2021-08-03 14:58:49 +0300

    add MovieList component

---
+++ src/components/MoviesList.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function MoviesList({ movies }) {
+  const displayMovies = Object.keys(movies).map((movieID) => (
+    <li key={movieID}>
+      <Link to={`/movie/${movieID}`}>{movies[movieID].title}</Link>
+    </li>
+  ));
+
+  console.log(displayMovies);
+
+  return (
+    <div>
+      <h1>Display Movies</h1>
+      <ul>{displayMovies}</ul>
+    </div>
+  );
+}
+
+export default MoviesList;
